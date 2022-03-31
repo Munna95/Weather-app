41486329f07c16280c08e34807e177ebc14d55e3
Luciana Sabrina Galera <sabrina.umb@gmail.com>
2022-03-31 14:37:28 -0300

    Added weather data from current location on page load

--- scripts.js
+++ scripts.js
@@ -1,9 +1,41 @@
 const key = "17d66de3484c7c276a19fba52f94d56e";
 const getLocationForm = document.querySelector("#locationForm");
 
-async function getWeatherData() {
+getLocationForm.addEventListener("submit", function (e) {
+  getWeatherData(getLocationForm.locationInput.value);
+  e.preventDefault();
+});
+
+window.addEventListener("load", function (e) {
+  getCoordinates();
+  e.preventDefault();
+});
+
+async function getCoordinates() {
+  if (navigator.geolocation) {
+    navigator.geolocation.getCurrentPosition(getCityName);
+  } else {
+    alert("Please enable geolocation");
+  }
+}
+
+async function getCityName(position) {
+  console.log(position.coords);
+  const lat = position.coords.latitude;
+  const lon = position.coords.longitude;
+
+
   let response = await fetch(
-    `http://api.openweathermap.org/data/2.5/weather?q=${getLocationForm.locationInput.value}&APPID=${key}`
+    `http://api.openweathermap.org/geo/1.0/reverse?lat=${lat}&lon=${lon}&limit=1&appid=${key}`
+  ).then((response) => response.json());
+    cityName = response[0].name
+    console.log(response[0])
+    getWeatherData(cityName);
+}
+
+async function getWeatherData(city) {
+  let response = await fetch(
+    `http://api.openweathermap.org/data/2.5/weather?q=${city}&APPID=${key}`
   ).then((response) => response.json());
   if (response.cod !== 200) {
     console.log("Invalid location"); //Here I should replace the console message for a modal or pop up notification
@@ -33,8 +65,3 @@ async function getWeatherData() {
     }
   }
 }
-
-getLocationForm.addEventListener("submit", function (e) {
-  getWeatherData();
-  e.preventDefault();
-});
