0676b2847b4845811a74ceb280e10c5a9b302996
Luciana Sabrina Galera <sabrina.umb@gmail.com>
2022-03-31 17:30:17 -0300

    Modified styles, added units to displayed data

--- scripts.js
+++ scripts.js
@@ -1,13 +1,14 @@
 const key = "17d66de3484c7c276a19fba52f94d56e";
 const getLocationForm = document.querySelector("#locationForm");
 
-getLocationForm.addEventListener("submit", function (e) {
-  getWeatherData(getLocationForm.locationInput.value);
+window.addEventListener("load", function (e) {
+  console.log("loaded");
+  getCoordinates();
   e.preventDefault();
 });
 
-window.addEventListener("load", function (e) {
-  getCoordinates();
+getLocationForm.addEventListener("submit", function (e) {
+  getWeatherData(getLocationForm.locationInput.value);
   e.preventDefault();
 });
 
@@ -20,22 +21,19 @@ async function getCoordinates() {
 }
 
 async function getCityName(position) {
-  console.log(position.coords);
   const lat = position.coords.latitude;
   const lon = position.coords.longitude;
-
 
   let response = await fetch(
     `http://api.openweathermap.org/geo/1.0/reverse?lat=${lat}&lon=${lon}&limit=1&appid=${key}`
   ).then((response) => response.json());
-    cityName = response[0].name
-    console.log(response[0])
-    getWeatherData(cityName);
+  cityName = response[0].name;
+  getWeatherData(cityName);
 }
 
 async function getWeatherData(city) {
   let response = await fetch(
-    `http://api.openweathermap.org/data/2.5/weather?q=${city}&APPID=${key}`
+    `http://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&humidity=humidity.unit&pressure=pressure.unit&APPID=${key}`
   ).then((response) => response.json());
   if (response.cod !== 200) {
     console.log("Invalid location"); //Here I should replace the console message for a modal or pop up notification
@@ -46,20 +44,22 @@ async function getWeatherData(city) {
   function createWeatherObject(data) {
     const weatherData = {
       name: data.name,
-      timezone: data.timezone,
+      timezone: "GMT" + data.timezone / 3600,
       main: data.weather[0].main,
-      description: data.weather[0].description,
-      actual_temp: data.main.temp,
-      temp_min: data.main.temp_min,
-      temp_max: data.main.temp_max,
-      pressure: data.main.pressure,
-      humidity: data.main.humidity,
+      actual_temp: data.main.temp.toFixed(1) + "°",
+      temp_min: data.main.temp_min.toFixed(1) + "°",
+      temp_max: data.main.temp_max.toFixed(1) + "°",
+      pressure: data.main.pressure + " hPa",
+      humidity: data.main.humidity + "%"
     };
-
-    displayWeatherData(weatherData);
+    const weatherIcon = data.weather[0].icon;
+    displayWeatherData(weatherData, weatherIcon);
   }
 
-  function displayWeatherData(data) {
+  function displayWeatherData(data, icon) {
+    document.getElementById(
+      "weatherImg"
+    ).src = `https://openweathermap.org/img/wn/${icon}@4x.png`;
     for (const key in data) {
       document.querySelector("." + key).innerText = `${data[key]}`;
     }
